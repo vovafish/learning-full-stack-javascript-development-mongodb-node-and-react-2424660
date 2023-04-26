90428f249ceb0737dc0b02ffb9ba8548117915c1
Vladimirs <vladimirribakov123@mail.ru>
2023-04-26 21:37:44 +0100

    using an API point from browser

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import {createRoot} from 'react-dom/client'
 import App from './components/app';
+import axios from "axios";
+import { API_SERVER_URL } from './public-config';
 
 
 const container = document.getElementById("app");
 const root = createRoot(container);
+
+axios.get(`${API_SERVER_URL}/contests`)
+    .then((resp) => {
+        console.log(resp);
+        
+    })
 
 root.render(<App/>);
--- src/server/config.ts
+++ src/server/config.ts
@@ -3,8 +3,6 @@ const env = process.env;
 export const PORT = env.PORT ?? "8080";
 export const HOST = env.HOST ?? "0.0.0.0";
 export const SERVER_URL = `http://${HOST}:${PORT}`;
-export const API_SERVER_URL = `http://${HOST}:${PORT}/api`;
-
 
 export const MONGODB_URI =
   env.MONGODB_URI ?? "mongodb://localhost:27017";
